4668aaab7813ad6036723008499398a26287da62
beesan-ahmad <abumfarreh@gmail.com>
2023-11-09 11:52:02 +0200

    Reading & Writing files synchronously in node.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const readline = require ('readline');
  const fs = require('fs');
 const { isUtf8 } = require('buffer');
 //  how to read input from the user
-
-const rl = readline.createInterface({
+/*const rl = readline.createInterface({
 input: process.stdin,
 output: process.stdout
 
@@ -17,4 +16,10 @@ rl.on('close',()=>{
     console.log('interface closed');
     process.exit(0);
 }
-);
+);*/
+// read from the file
+let textIn = fs.readFileSync('./Files/input.txt' ,'utf-8');
+console.log(textIn);
+// write from the file
+let content = `data read from input file:${textIn} \nDate created: ${new Date()}` ;
+fs.writeFileSync('./Files/output.txt',content);
