dc2a00821d3043f9172338ba4802fab7ac10a3c3
beesan-ahmad <abumfarreh@gmail.com>
2023-11-12 10:21:00 +0200

    Reading and writing from the file asynchronously in node.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { listeners } = require('process');
 const readline = require ('readline');
  const fs = require('fs');
 const { isUtf8 } = require('buffer');
+//****************************** */
 //  how to read input from the user
 /*const rl = readline.createInterface({
 input: process.stdin,
@@ -16,10 +17,30 @@ rl.on('close',()=>{
     console.log('interface closed');
     process.exit(0);
 }
-);*/
-// read from the file
+);
+//*************************** 
+// reading  from the file synchronously
 let textIn = fs.readFileSync('./Files/input.txt' ,'utf-8');
 console.log(textIn);
-// write from the file
+// writing from the file synchronously
 let content = `data read from input file:${textIn} \nDate created: ${new Date()}` ;
-fs.writeFileSync('./Files/output.txt',content);
+fs.writeFileSync('./Files/output.txt',content);*/
+//****************************
+//Reading and writing from the file asynchronously
+fs.readFile('./Files/start.txt','utf-8',
+ (error1,data1)=> {
+    console.log(data1)
+    fs.readFile(`./Files/${data1}.txt`,'utf-8',(error2,data2)=>{//read the content of input.txt file
+        console.log(data2);
+        fs.readFile(`./Files/append.txt`,'utf-8',(error3,data3)=>{
+            console.log(data3);
+            fs.writeFile(`./Files/output.txt`,`${data2}\n\n${data3}\n\nDate created: ${new Date()}`,()=>{
+                console.log('file written successfully');
+            })
+        })
+    })
+});/*we see the shape of the code looks like triangle that means we have a callback help(
+when we call a callback function inside another callback function...etc we can avoid it by
+using async await or promise
+)*/
+console.log('Reading File......');
